df12c6f07cfab874dc3b46e5521dcea9b997f818
firesquid6 <jdeiss06@gmail.com>
2023-12-17 22:06:17 -0600

    add scaffolding for server function

--- lib/server.ts
+++ lib/server.ts
@@ -1,15 +1,18 @@
 import Elysia from "elysia";
 import { html } from "@elysiajs/html";
 import { InkdocsConfig } from "./config";
+import { Filesystem, getRealFilesystem } from "./files";
 
 export function inkdocsServe(config: InkdocsConfig) {
   const app = new Elysia();
   const port = getPort(config);
+  const outputFolder = config.outputFolder ?? "build";
+  const filesystem = getRealFilesystem(outputFolder);
 
   app.use(html());
   app.get("*", ({ params }) => {
     const route = params["*"];
-    return getHtmlFromRoute(route, config);
+    return getResponseFromRoute(route, outputFolder, filesystem);
   });
 
   app.listen(port, () => {
@@ -23,4 +26,31 @@ function getPort(config: InkdocsConfig) {
   return config.devOptions?.port ?? 3000;
 }
 
-function getHtmlFromRoute(route: string, config: InkdocsConfig) {}
+function getResponseFromRoute(
+  route: string,
+  outputFolder: string,
+  filesystem: Filesystem,
+) {}
+
+export function getFilepathFromRoute(
+  route: string,
+  outputFolder: string,
+  filesystem: Filesystem,
+) {
+  route = outputFolder + route;
+  if (route.split(".").length > 1 && !route.endsWith(".html")) {
+    return route;
+  }
+}
+
+export function getPossibleFilepaths(
+  filepath: string,
+  buildDir: string,
+  filesystem: Filesystem,
+) {
+  const possibleFilepath = [filepath, `${filepath}/index.html`];
+  if (filepath === "/") {
+    possibleFilepath.push(`${filepath}index.html`);
+  }
+  return possibleFilepath.map((filepath) => `${buildDir}${filepath}`);
+}
