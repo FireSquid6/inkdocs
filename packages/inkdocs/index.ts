f462f4156fabea7158a1aaf8b9725079d151b6f5
firesquid6 <jdeiss06@gmail.com>
2023-12-21 19:59:52 -0600

    add minimal example

--- packages/inkdocs/index.ts
+++ packages/inkdocs/index.ts
@@ -1,10 +1,22 @@
+import "@kitajs/html/register";
+
 export interface InkdocsOptions {
   builders?: Builder[];
+  staticFolder?: string | undefined;
+  buildFolder?: string | undefined;
+  contentFolder: string;
 }
 
-export const defaultOptions: InkdocsOptions = {};
+export const defaultOptions: InkdocsOptions = {
+  contentFolder: "content",
+  buildFolder: "build",
+};
 
 export interface Builder {
   filetypes: string[]; // filetypes that the
-  build(content: string): Promise<string>;
+  build(content: string): Promise<BuildResult> | BuildResult;
+}
+
+export interface BuildResult {
+  html: JSX.Element;
 }
