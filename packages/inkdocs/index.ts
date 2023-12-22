c458b2f0998746eb9faa7e75369a97ac0c57eeac
firesquid6 <jdeiss06@gmail.com>
2023-12-21 19:53:40 -0600

    start work on base

--- packages/inkdocs/index.ts
+++ packages/inkdocs/index.ts
@@ -1,1 +1,10 @@
-console.log("Hello via Bun!");
+export interface InkdocsOptions {
+  builders?: Builder[];
+}
+
+export const defaultOptions: InkdocsOptions = {};
+
+export interface Builder {
+  filetypes: string[]; // filetypes that the
+  build(content: string): Promise<string>;
+}
