ebb6359ebbbe4a926b38b9cfb81c3c20625ce681
firesquid6 <jdeiss06@gmail.com>
2023-12-29 20:04:10 -0600

    add build function

--- packages/inkdocs/builder/index.ts
+++ packages/inkdocs/builder/index.ts
@@ -1,12 +1,13 @@
 import { InkdocsOptions } from "..";
 import { convertHtmlFiles } from "./html_converter";
-import { realFilesystem } from "../lib/filesystem";
-import { realLogger } from "../lib/logger";
+import { Filesystem, realFilesystem } from "../lib/filesystem";
+import { Logger, realLogger } from "../lib/logger";
 
-export function build(options: InkdocsOptions): void {
-  const filesystem = realFilesystem();
-  const logger = realLogger();
-
+export function build(
+  options: InkdocsOptions,
+  filesystem: Filesystem = realFilesystem(),
+  logger: Logger = realLogger(),
+): void {
   const htmlFiles = convertHtmlFiles(options, filesystem, logger);
 
   for (const [filepath, html] of htmlFiles) {
