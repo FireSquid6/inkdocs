d083ce640bb658fff617963f25aa626d0a774e89
firesquid6 <jdeiss06@gmail.com>
2024-01-04 12:28:37 -0600

    add todo for component router

---
+++ packages/inkdocs/plugins/component_router.ts
@@ -0,0 +1,2 @@
+// todo
+// allows the user to add everything in a component folder as api routes under @component/*
--- packages/inkdocs/plugins/swap_router.ts
+++ packages/inkdocs/plugins/swap_router.ts
@@ -1,10 +1,12 @@
 // To use this plugin, you must:
 import { Parser, Plugin } from "..";
+import { ApiRoute } from "../server";
 
 // TODO
 // This plugin assumes that the user has installed htmx into the head of their base html.
 // This plugin also can only deal with markdown. Any other file type must have a custom parser written.
 export default function lazyRouter(): Plugin {
+  let apiRoutes: ApiRoute[] = [];
   return {
     beforeBuild: () => {
       const parsers = new Map<string, Parser>();
