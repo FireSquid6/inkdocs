25527966d843b009cfa3f6ec48361188687ec5bd
firesquid6 <jdeiss06@gmail.com>
2024-01-17 19:55:20 -0600

    add more to the client

--- packages/devserver/client-javascript.js
+++ packages/devserver/client-javascript.js
@@ -1,1 +1,10 @@
-console.log("Hello from client-javascript.js");
+let version = 0;
+
+setInterval(async () => {
+  const response = await fetch("http://localhost:8008/version", {
+    mode: "no-cors",
+    crossorigin: true,
+    cache: "no-store",
+  });
+  console.log(response);
+}, 1000);
--- packages/devserver/index.ts
+++ packages/devserver/index.ts
@@ -1,16 +1,51 @@
 import Elysia from "elysia";
 import path from "path";
+import { watch } from "fs";
+import { cors } from "@elysiajs/cors";
+import { html } from "@elysiajs/html";
+import "@kitajs/html/register";
+import fs from "fs";
 
 const app = new Elysia();
+app.use(html());
 
+// TODO:
+// - add a build folder option in the cli args to avoid watching
+// - make interface to give devserver access to what commands to run for building and serving
+// - maybe a devserver config file?
+let serverPid = -1;
 let version = 0;
+
 app.get("/client-javascript", () => {
   return Bun.file(path.join(__dirname, "client-javascript.js"));
 });
 app.get("/version", () => {
-  return {
-    version: version,
-  };
+  console.log("version requested");
+  return version;
+});
+// TODO: remove this
+app.get("/test", () => {
+  const file = fs.readFileSync("test-html.html", "utf-8");
+  console.log(file);
+  return file as JSX.Element;
 });
 
-app.listen(8008);
+app.listen(8008, () => {
+  console.log("listening on port 8008");
+});
+
+watch(
+  // TODO: change this to a real thing
+  "test-files",
+  { recursive: true },
+  () => {
+    console.log("change detected. Increasing Version.");
+    // TODO: this should:
+    // - kill the old server process
+    // - run the build
+    // - start the new server
+    // - FINALLY, increase version
+    version += 1;
+    console.log(version);
+  },
+);
