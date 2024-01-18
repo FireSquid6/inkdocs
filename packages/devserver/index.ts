84255ed84f2952328d54da05db8ee5c1b4f40191
firesquid6 <jdeiss06@gmail.com>
2024-01-17 19:19:35 -0600

    add client javascript for devserver

--- packages/devserver/index.ts
+++ packages/devserver/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello via Bun!");
+import Elysia from "elysia";
+import path from "path";
+
+const app = new Elysia();
+
+let version = 0;
+app.get("/client-javascript", () => {
+  return Bun.file(path.join(__dirname, "client-javascript.js"));
+});
+app.get("/version", () => {
+  return {
+    version: version,
+  };
+});
+
+app.listen(8008);
