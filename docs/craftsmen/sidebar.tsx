f601af7d382b106a83a76a1ad842bfa29283c399
firesquid6 <jdeiss06@gmail.com>
2024-01-22 14:49:47 -0600

    add swap link to sidenav

--- docs/craftsmen/sidebar.tsx
+++ docs/craftsmen/sidebar.tsx
@@ -1,10 +1,18 @@
 import { Artifact, InkdocsOptions, Route } from "inkdocs";
+import SwapLink from "inkdocs/components/SwapLink";
 
 export default function Sidebar(
   options: InkdocsOptions,
   routes: Route[],
 ): Artifact {
-  const sidebar = <div class="sidebar">{routes.map((route) => { })}</div>;
+  const sidenav = (
+    <div class="sidenav">
+      {routes.map((route) => {
+        return;
+        <SwapLink>{route.metadata.title ?? route.filepath}</SwapLink>;
+      })}
+    </div>
+  );
   return {
     name: "sidebar",
     data: sidebar,
