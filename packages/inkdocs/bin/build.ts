88584d772a5ff134ab6c7ec0234499f1621e5d99
firesquid6 <jdeiss06@gmail.com>
2024-01-24 20:57:28 -0600

    remvoe unused bin scripts

--- packages/inkdocs/bin/build.ts
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-import { getOptions } from "../lib/get-options";
-import { build } from "../builder";
-
-build(await getOptions());
--- packages/inkdocs/bin/serve.ts
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-import { getOptions } from "../lib/get-options";
-import { serve } from "../server";
-
-serve(await getOptions());
