f148789d393f5160f890a0a096d498ee717a8c98
firesquid6 <jdeiss06@gmail.com>
2024-01-30 13:25:48 -0600

    add some more helpful comments to shell.nix

--- shell.nix
+++ shell.nix
@@ -1,7 +1,17 @@
+# If you're a non-nix user, you'll need:
+# - bun (whatever the latest version is)
+# - node js version 20
+# - flyctl (if you plan on deploying to fly.io)
+# - libgcc is just there because some neovim plugins need it
 let
   unstable = import (fetchTarball https://nixos.org/channels/nixos-unstable/nixexprs.tar.xz) { };
 in
 { nixpkgs ? import <nixpkgs> {} }:
 with nixpkgs; mkShell {
-  buildInputs = [ unstable.bun nodejs_20 flyctl ];
+  buildInputs = [ 
+    unstable.bun 
+    nodejs_20
+    flyctl 
+    libgcc
+  ];
 }
