cf894cf9bb3b277771fd4ed8509e13cd6e98f4c6
firesquid6 <jdeiss06@gmail.com>
2024-01-30 17:27:18 -0600

    fix the devserver

--- making-watcher-fail/killingmyself.ts
+++ making-watcher-fail/killingmyself.ts
@@ -1,6 +1,10 @@
 import Watcher from "watcher";
 
-const watcher = new Watcher(".");
-watcher.on("all", () => {
-  console.log("I detected something");
+const watcher = new Watcher(".", { recursive: true });
+watcher.on("change", (filepath) => {
+  console.log("I saw an update: ");
+  console.log(filepath);
+});
+watcher.on("error", (error) => {
+  console.log(error);
 });
