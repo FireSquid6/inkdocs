0ce51b68e3a5e7f18324536f0f881bb444bd0008
firesquid6 <jdeiss06@gmail.com>
2024-01-30 13:48:10 -0600

    remove folder from main

--- making-watcher-fail/serve.ts
+++
@@ -1,9 +0,0 @@
-import { Elysia } from "elysia";
-
-const app = new Elysia();
-app.get("/", () => {
-  return "Hello world!";
-});
-app.listen(8008, () => {
-  console.log("listening on port 8008");
-});
