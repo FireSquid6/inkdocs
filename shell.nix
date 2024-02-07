d2d8c3efb213271bcd568a669da4c2c21df3b96e
firesquid6 <jdeiss06@gmail.com>
2024-02-07 17:31:11 -0600

    fix fetch tarball in nix shell

--- shell.nix
+++ shell.nix
@@ -3,8 +3,16 @@
 # - node js version 20
 # - flyctl (if you plan on deploying to fly.io)
 # - libgcc is just there because some neovim plugins need it
+
+# Help! I'm a nix user and it isn't working!
+#
+# try running these commands:
+# sudo nix-channel --add https://nixos.org/channels/nixos-unstable nixos-unstable
+# sudo nix-channel --update
+# then run nix-shell again
+
 let
-  unstable = import (fetchTarball https://nixos.org/channels/nixos-unstable/nixexprs.tar.xz) { };
+  unstable = import <nixos-unstable> { config = { allowUnfree = true; }; };
 in
 { nixpkgs ? import <nixpkgs> { } }:
 with nixpkgs; mkShell {
