56292ab7fe6b5ff36846ec953e959dbe124e1748
firesquid6 <jdeiss06@gmail.com>
2024-02-09 10:41:41 -0600

    add blog

--- docs/layouts/blog.tsx
+++ docs/layouts/blog.tsx
@@ -1,12 +1,18 @@
 import { Layout } from "inkdocs";
+import Root from "../components/Root";
 
 const BlogLayout: Layout = (children, metadata) => {
   return (
-    <article id="content" class="prose prose-invert">
-      <title>{metadata.title}</title>
-      <h1>{metadata.title}</h1>
-      <div>{children}</div>
-    </article>
+    <Root>
+      <>
+        <article id="content" class="prose prose-invert mx-auto">
+          <title>{metadata.title}</title>
+          <h1 class="text-center">{metadata.title}</h1>
+          <hr />
+          <div>{children}</div>
+        </article>
+      </>
+    </Root>
   );
 };
 
