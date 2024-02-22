a18d627c525e9e620b5b27c0cffa85952a954cd4
firesquid6 <jdeiss06@gmail.com>
2024-02-22 13:14:47 -0600

    remove space

--- docs/inkdocs.ts
+++ docs/inkdocs.ts
@@ -11,7 +11,6 @@ import { devserverPlugin } from "inkdocs-server";
 
 export function getOptions(): InkdocsOptions {
   const baseHtml = `<html>
-
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
